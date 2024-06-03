a47066561d27d681fb51023d4d5c24ab54ce3706
ValentinGrenet <valen2106@free.fr>
2024-06-03 18:07:36 +0200

    New script and new part in Repeat for closest gene

---
+++ Find_genes.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Directories
+general_dir="/home/valentin-grenet/Bureau/Données/TE_sequences"
+data_dir="Repeat_TEs"
+
+# Files
+Coordinates_file="TE_coordinates.bed"
+Coordinates_sorted="TE_coord_sorted.bed"
+Result_file="TE_closest_genes.bed"
+Genes_file="$general_dir/Genes_coord_sorted.bed"
+
+cd $general_dir
+
+mamba activate tools
+for consensus in consensus*
+do
+	cd $consensus/$data_dir
+    sed -i 's/^/chr/; s/chrchr/chr/g' $consensus.TE_coordinates.bed
+
+	sort-bed $consensus.$Coordinates_file > $consensus.$Coordinates_sorted
+    bedtools closest -a $consensus.$Coordinates_sorted \
+                     -b $Genes_file -D ref > $consensus.$Result_file
+
+	cd ../..
+done
+mamba deactivate
--- Formating_nexus_files.sh
+++ Formating_nexus_files.sh
@@ -2,12 +2,17 @@
 
 cd /home/valentin-grenet/Bureau/Données/TE_sequences
 
-for consensus in consensus_Cluster_103_subfam_2
+for consensus in consensus*
 do
 	cd $consensus/Repeat_TEs
+
+	mamba activate tools
+	seqmagick convert --output-format nexus --alphabet dna \
+	$consensus.LTR_trimmed.fasta $consensus.LTR_trimmed.nex
+
 	sed -i '/TITLE Taxa;/d' $consensus.LTR_trimmed.nex
 	sed -i '/TITLE  Character_Matrix;/d' $consensus.LTR_trimmed.nex
-	sed -i "s/NTAX=/NTAX= /g" $consensus.LTR_trimmed.nex
-	sed -i "s/#LTR:Gypsy//g; s/#LTR:Copia//g; s/NCHAR=/NCHAR= /g; s/'//g; s/(+)//g; s/(-)//g" $consensus.LTR_trimmed.nex
+	sed -i "s/ntax=/ntax= /g; s/nchar=/nchar= /g" $consensus.LTR_trimmed.nex
+	sed -i "s/#LTR:Gypsy//g; s/#LTR:Copia//g; s/'//g; s/(+)//g; s/(-)//g" $consensus.LTR_trimmed.nex
 	cd ../..
 done
