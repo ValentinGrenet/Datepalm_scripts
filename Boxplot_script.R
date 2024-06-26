dfd415e201d6ad7a66ca25b954798499ae747386
ValentinGrenet <valen2106@free.fr>
2024-06-26 14:51:27 +0200

    Script for age extrcation and distance from genes

---
+++ Boxplot_script.R
@@ -0,0 +1,81 @@
+# Load necessary libraries
+library(ggplot2)
+library(dplyr)
+
+# Read the data
+setwd(dir = "/home/valentin-grenet/Bureau/Résultats/Insertion_ages/")
+pwd <- getwd()
+consensi <- list.files(path = "./", pattern = "consensus")
+data <- read.csv("Phoenix_dactylifera.LTR_all_ages.tsv", sep="\t")
+
+# Convert the 'estimated_age' column to numeric (replacing commas with dots)
+data$estimated_age <- as.numeric(gsub(",", ".", data$estimated_age))
+
+# Get the list of families
+families <- unique(data$family)
+
+# Generate boxplots for each family
+for (family in families) {
+  # Filter the data for the current family
+  family_data <- data %>% filter(family == !!family)
+  
+  # Create the boxplot
+  p <- ggplot(family_data, aes(x = consensus, y = estimated_age)) +
+    geom_boxplot() +
+    labs(title = paste("Boxplot for Family:", family), x = "Consensus", y = "Estimated Age") +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  print(p)
+  
+  ## Details :
+  # geom_boxplot() -> used to add a boxplot in the figure
+  # labs() -> define plot labels, like the title, the x and y legends
+  # theme() -> personnalise graph apparence
+  
+  # Save the plot
+  ggsave(filename = paste0("boxplot_", family, ".png"), plot = p, width = 10, height = 6)
+}
+
+# for median ages for Copia and Gypsy
+
+# Load necessary libraries
+library(ggplot2)
+library(dplyr)
+
+# Read the data
+setwd(dir = "/home/valentin-grenet/Bureau/Données")
+pwd <- getwd()
+consensi <- list.files(path = "./", pattern = "consensus")
+data <- read.csv("Phoenix_dactylifera.LTR_ages_median.tsv", sep="\t")
+
+# Convert the 'estimated_age' column to numeric (replacing commas with dots)
+data$age_consensus <- as.numeric(gsub(",", ".", data$age_consensus))
+
+# Get the list of families
+lineage <- unique(data$family)
+
+# Create the boxplot
+p <- ggplot(data, aes(x = data$family, y = age_consensus, fill = lineage)) +
+  geom_boxplot() +
+  labs(y = "Estimation de l'âge médian d'insertion (en My)") +
+  scale_fill_manual(values = c("Copia" = "orange", "Gypsy" = "yellow")) +
+  facet_wrap(~ lineage, scales = "free_x") +
+  theme(
+    panel.background = element_rect(fill = "white"),
+    panel.grid.major.y = element_line(color = "grey40"),
+    panel.grid.minor.y = element_line(color = "grey40", linetype = "dashed"),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    axis.line = element_line(color = "black"),
+    axis.text = element_text(size = 20), # Taille du texte des axes
+    axis.title = element_text(size = 20), # Taille du texte des axes
+    strip.text = element_text(size = 30)
+  )
+print(p)
+  
+    ## Details :
+  # geom_boxplot() -> used to add a boxplot in the figure
+  # labs() -> define plot labels, like the title, the x and y legends
+  # theme() -> personnalise graph apparence
+  
+# Save the plot
+ggsave(filename = paste0("boxplot_", family, ".png"), plot = p, width = 10, height = 6)
--- Tree_ages_export.R
+++ Tree_ages_export.R
@@ -7,34 +7,135 @@ consensi <- list.files(path = "./", pattern = "consensus")
 ## Get trees in a hash object
 trees <- hash()         # equivalent to dictionnary
 for (consensus in consensi) {
-  wd <- paste("./", consensus, sep = "")      # concatenate str
+  wd <- paste("./", consensus, "/Repeat_TEs", sep = "")      # concatenate str
   setwd(wd)
-  treename <- paste(consensus, ".LTR_alignment.nex.con.tre", sep = "")
+  treename <- paste(consensus, ".LTR_trimmed.nex.con.tre", sep = "")
   trees[[consensus]] <- ape::read.nexus(treename)
   setwd(pwd)
 }
 
 ## Begin ages extraction
-branch_lengths <- hash()
 id_LTRs <- hash()
+branch_lengths <- hash()
+age_LTRs <- hash()
 for (consensus in consensi) {
   tree <- trees[[consensus]]
   id_LTRs[[consensus]] <- tree$tip.label
   ## first get the node numbers of the tips
   nodes<-sapply(tree$tip.label, function(x,y) which(y==x),y=tree$tip.label)
   ## then get the edge lengths for those nodes
-  branch_lengths[[consensus]]<-setNames(tree$edge.length[sapply(nodes,
+  branch_lengths[[consensus]] <- setNames(tree$edge.length[sapply(nodes,
     function(x,y) which(y==x),y=tree$edge[,2])],names(nodes))
+  age_LTRs[[consensus]] <- branch_lengths[[consensus]] / 2.61E-3
 }
 
-## Write ages in tsv files
-table <- data.frame()
+## Write all ages in tsv files
+final_tsv <- data.frame(consensus, lineage_table[[consensus]], family_table[[consensus]], LTR, branch_lengths[[consensus]][[LTR]], age)
+colnames(final_tsv) <- c("consensus","lineage","family","LTR","median length","mean age")
+namefile <- paste("Phoenix_dactilfera.LTR_ages.tsv", sep = "")
+write.table(final_tsv, file = namefile, row.names = FALSE, sep = "\t", dec = ",")
 for (consensus in consensi) {
+  table <- data.frame()
+  print(consensus)
+  for (LTR in id_LTRs[[consensus]]) {
+    age <- branch_lengths[[consensus]][[LTR]]/2.61E-3       # obtained with Gaut et al., 1996 : Substitution rate comparisons between grasses and palms
+    line <- data.frame(consensus, lineage_table[[consensus]], family_table[[consensus]], LTR, branch_lengths[[consensus]][[LTR]], age)
+    table <- rbind(table, line)
+  }
+  write.table(table, file = namefile, row.names = FALSE, col.names = FALSE, append = TRUE,sep = "\t", dec = ",")
+}
+
+## Write median/mean ages in tsv files
+final_csv <- data.frame()
+for (consensus in consensi) {
+  table <- data.frame()
   for (LTR in id_LTRs[[consensus]]) {
     line <- data.frame(consensus, LTR, branch_lengths[[consensus]][[LTR]])
     table <- rbind(table, line)
   }
+  mean_consensus <- mean(table$branch_lengths..consensus....LTR..)
+  age_consensus <- mean_consensus/2.61E-3       # obtained with Gaut et al., 1996 : Substitution rate comparisons between grasses and palms
+  new_mean <- data.frame(consensus, mean_consensus, age_consensus)
+  final_csv <- rbind(final_csv, new_mean)
 }
-colnames(table) <- c("consensus","id LTR", "branch length")
-namefile <- paste("Phoenix_dactylifera.LTR_ages.csv", sep = "")
-write.csv(table, file = namefile, row.names = FALSE)
+colnames(table) <- c("consensus","median length", "mean age")
+namefile <- "Phoenix_dactylifera.LTR_ages_mean.tsv"
+write.table(final_csv, file = namefile, row.names = FALSE, sep = "\t", dec = ",")
+
+
+
+## Classifications
+setwd(dir = "/home/valentin-grenet/Bureau/Données/")
+classif_tsv <- read.table("Phoenix_dactylifera.LTR_ages_median.tsv", header = TRUE)
+lineage_table <- hash()
+family_table <- hash()
+for (i in 1:nrow(classif_tsv)) {
+  line <- classif_tsv[i,]
+  print(line)
+  lineage_table[[line$consensus]] = line$lineage
+  family_table[[line$consensus]] = line$family
+}
+final_tsv <- data.frame()
+for (consensus in consensi) {
+  for (LTR in id_LTRs[[consensus]]) {
+    new_line <- data.frame(consensus, lineage_table[[consensus]], family_table[[consensus]], 
+                           branch_lengths[[consensus]][[LTR]], age_LTRs[[consensus]][[LTR]])
+    final_tsv <- rbind(final_tsv, new_line)
+  }
+}
+colnames(final_tsv) <- c("consensus","lineage","family","branch_length", "estimated_age")
+namefile <- "Phoenix_dactylifera.LTR_all_ages.tsv"
+write.table(final_tsv, file = namefile, row.names = FALSE, sep = "\t", dec = ",")
+
+## Write boxplot ages
+family_data <- subset(final_tsv, family == "SIRE")
+boxplot(family_data$consensus~family_data$estimated_age)
+
+
+## Calculate young LTRs proportion
+
+# Lire le fichier avec les valeurs séparées par des tabulations et les nombres décimaux avec des virgules
+file_path <- "Phoenix_dactilfera.LTR_ages.tsv"
+data <- read.csv(file_path, sep = "\t", dec = ",", header = TRUE)
+
+# Charger les bibliothèques nécessaires
+library(dplyr)
+library(ggplot2)
+
+# Ajouter une colonne pour indiquer si mean age < 1
+data <- data %>% mutate(mean_age_below_1 = ifelse(mean.age < 3, 1, 0))
+
+# Ajouter une colonne pour les couleurs
+data <- data_proportions %>% mutate(color = ifelse(lineage == "Copia", "orange", ifelse(lineage == "Gypsy", "yellow", "grey")))
+
+# Calculer la proportion pour chaque famille et le nombre d'éléments
+family_stats <- data %>% 
+  group_by(family, color) %>% 
+  summarise(
+    proportion_below_1 = mean(mean_age_below_1),
+    count = n()
+  ) %>%
+  arrange(count)  # Trier par nombre d'éléments
+
+# Afficher les résultats
+print(family_stats)
+
+# Créer le graphique
+ggplot(family_stats, aes(x = reorder(family, count), y = proportion_below_1, fill = color)) +
+  geom_bar(stat = "identity") +
+  geom_line(aes(y = count / max(count), group = 1, color = "Nombre d'éléments"), size = 1) +
+  scale_y_continuous(
+    name = "Proportion de LTR avec un âge d'insertion < 3 Ma",
+    sec.axis = sec_axis(~ . * max(family_stats$count), name = "Nombre d'éléments")
+  ) +
+  scale_fill_manual(values = c("orange", "yellow", "grey")) +
+  scale_color_manual(values = c("Nombre d'éléments" = "red")) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  labs(
+    title = "Proportion de mean age < 1 et nombre d'éléments par famille",
+    x = "Famille",
+    y = "Proportion de mean age < 1"
+  ) +
+  theme(legend.position = "top") +
+  guides(fill = "none")
